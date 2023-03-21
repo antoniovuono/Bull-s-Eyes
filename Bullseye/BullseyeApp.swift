412550c3893b9a1f3ae1b652536cad377872db0b
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2023-03-21 16:08:21 -0300

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Antonio Vuono on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
